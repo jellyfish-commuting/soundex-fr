b78ecbf20860aa7665df60c3b2500afee8d75a07
Mohamed B <mohamed@jellyfish-commuting.com>
2020-03-26 14:00:35 +0100

    Remove accents before compute soundex

--- index.test.js
+++ index.test.js
@@ -4,7 +4,7 @@ const soundex = require('./index');
 describe('Soundex FR', () => {
   it('expect equal for KABYLE & KAPILE', () => expect(soundex('KABYLE')).toBe(soundex('KAPILE')));
   it('expect equal for AMIE & ANIE', () => expect(soundex('AMIE')).toBe(soundex('ANIE')));
-  it('expect equal for PENIBLE & PEMIPLE', () => expect(soundex('PENIBLE')).toBe(soundex('PEMIPLE')));
+  it('expect equal for PÉNIBLE & PEMIPLE', () => expect(soundex('PENIBLE')).toBe(soundex('PEMIPLE')));
   it('expect equal for ACCROCHAGE & AKROCHAJE', () => expect(soundex('ACCROCHAGE')).toBe(soundex('AKROCHAJE')));
   it('expect different for DEUX & TROIS', () => expect(soundex('DEUX')).not.toBe(soundex('TROIS')));
 });
