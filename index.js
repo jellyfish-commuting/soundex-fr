e3bca66aea80f24075e3d1df7bc54918b4dd0ae9
Mohamed B <mohamed@jellyfish-commuting.com>
2020-03-26 12:48:01 +0100

    Soundex for french phonetics

---
+++ index.js
@@ -0,0 +1,51 @@
+//-------
+// Soundex for french phonetics
+//-------
+module.exports = function (str) {
+  // Keep letters only
+  const buffer = String(str).toUpperCase().replace(/[^A-Z]/g, '');
+
+  // Empty string ?
+  if (buffer.length === 0) {
+    return null;
+  }
+
+  // Letter mapping for french phonetics
+  const codes = {
+    B: 1,
+    P: 1,
+    C: 2,
+    K: 2,
+    Q: 2,
+    D: 3,
+    T: 3,
+    L: 4,
+    M: 5,
+    N: 5,
+    R: 6,
+    G: 7,
+    J: 7,
+    W: 8,
+    Z: 8,
+    S: 8,
+    F: 9,
+    V: 9,
+  };
+
+  // Init loop
+  const first = buffer.charAt(0);
+  let result = '';
+  let prev = null;
+
+  for (let i = 1; i < buffer.length; i += 1) {
+    const code = codes[buffer[i]] || null;
+
+    if (code !== null && (i !== 1 || code !== codes[first]) && code !== prev) {
+      result += code;
+    }
+
+    prev = code;
+  }
+
+  return `${first}${result}000`.slice(0, 4);
+};
---
+++ index.test.js
@@ -0,0 +1,10 @@
+const soundex = require('./index');
+
+// Start tests ...
+describe('Soundex FR', () => {
+  it('expect equal for KABYLE & KAPILE', () => expect(soundex('KABYLE')).toBe(soundex('KAPILE')));
+  it('expect equal for AMIE & ANIE', () => expect(soundex('AMIE')).toBe(soundex('ANIE')));
+  it('expect equal for PENIBLE & PEMIPLE', () => expect(soundex('PENIBLE')).toBe(soundex('PEMIPLE')));
+  it('expect equal for ACCROCHAGE & AKROCHAJE', () => expect(soundex('ACCROCHAGE')).toBe(soundex('AKROCHAJE')));
+  it('expect different for DEUX & TROIS', () => expect(soundex('DEUX')).not.toBe(soundex('TROIS')));
+});
