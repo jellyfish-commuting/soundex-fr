ac5b20e4696e0d77f70e211a2aa9cffc1c255215
Mohamed B <mohamed@jellyfish-commuting.com>
2020-04-01 22:39:27 +0200

    Update test cases

--- index.test.js
+++ index.test.js
@@ -2,10 +2,11 @@ const soundex = require('./index');
 
 // Start tests ...
 describe('Soundex FR', () => {
-  it('expect equal for Kabyle & Kapile', () => expect(soundex('Kabyle')).toBe(soundex('Kapile')));
   it('expect equal for Dupont & Dupond', () => expect(soundex('Dupont')).toBe(soundex('Dupond')));
   it('expect equal for Éric & Erick', () => expect(soundex('Éric')).toBe(soundex('Erick')));
   it('expect equal for Robert & Rupert', () => expect(soundex('Robert')).toBe(soundex('Rupert')));
   it('expect equal for Annie & Amnie', () => expect(soundex('Annie')).toBe(soundex('Amnie')));
+  it('expect equal for Jean-Jacque & Jan-Jaque', () => expect(soundex('Jean-Jacque')).toBe(soundex('Jan-Jaque')));
+  it('expect M6358 for Martinez with length = 5', () => expect(soundex('Martinez', 5)).toBe('M6358'));
   it('expect different for Kabyle & Arab', () => expect(soundex('Kabyle')).not.toBe(soundex('Arab')));
 });
