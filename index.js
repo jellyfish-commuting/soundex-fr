39ab574be001ecafc7c0cb4fdc8d1f53753ff809
Mohamed B <mohamed@jellyfish-commuting.com>
2020-04-01 22:39:19 +0200

    Add length param

--- index.js
+++ index.js
@@ -1,44 +1,54 @@
-//-------
-// Soundex for french phonetics
-//-------
-module.exports = function (str) {
-  // Keep letters only
-  const buffer = String(str).normalize('NFD').toUpperCase().replace(/[\u0300-\u036f]|[^A-Z]/g, '');
+// Letter mapping for french phonetics, see https://fr.wikipedia.org/wiki/Soundex#Description
+const codes = {
+  B: 1, P: 1,
+  C: 2, K: 2, Q: 2,
+  D: 3, T: 3,
+  L: 4,
+  M: 5, N: 5,
+  R: 6,
+  G: 7, J: 7,
+  X: 8, Z: 8, S: 8,
+  F: 9, V: 9,
+  A: 0, E: 0, I: 0, O: 0, U: 0,
+};
 
-  // Empty string ?
-  if (!buffer.length) {
-    return '0000';
+//-------
+// Soundex
+//-------
+module.exports = function (str, length = 4) {
+  // Capitalize and remove accents
+  const buffer = String(str).normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase();
+
+  // Init result
+  let result = buffer.charAt(0);
+  let previous = codes[result];
+
+  // Map all chars
+  for(let i = 1; i < buffer.length; i += 1) {
+    // Retrieve current values
+    const letter = buffer.charAt(i);
+    const code = codes[letter];
+
+    // Ignore non letter character and apply the rule:
+    // "two letters with the same number separated by 'h' or 'w' are coded as a single number"
+    if (code === undefined || letter === 'H' || letter === 'W') {
+      continue;
+    }
+
+    // Append code if different than previous
+    if(code && code !== previous) {
+      result += code;
+    }
+
+    // Soundex key is fulfilled ?
+    if (result.length === length) {
+      return result;
+    }
+
+    // Save previous for next loop
+    previous = code;
   }
 
-  // Letter mapping for french phonetics
-  const codes = {
-    B: '1', P: '1',
-    C: '2', K: '2', Q: '2',
-    D: '3', T: '3',
-    L: '4',
-    M: '5', N: '5',
-    R: '6',
-    G: '7', J: '7',
-    X: '8', Z: '8', S: '8',
-    F: '9', V: '9',
-  };
-
-  return [...buffer.slice(1)]
-    // Map all chars in buffer
-    .reduce(
-      (acc, letter, i) => {
-        // Different than previous letter ?
-        if (codes[letter] && codes[letter] !== codes[buffer[i]]) {
-          return acc + codes[letter];
-        }
-
-        return acc;
-      },
-      // Init accumulator with first letter
-      buffer[0],
-    )
-    // Fill with 0
-    .padEnd(4, '0')
-    // Return the four first chars
-    .slice(0, 4);
+  // Fill with 0
+  return result.padEnd(length, '0');
 };
