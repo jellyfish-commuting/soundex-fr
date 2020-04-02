3a8176aa99e91f92a19881fa7f0a70b9db58a86c
Mohamed B <mohamed@jellyfish-commuting.com>
2020-04-02 10:57:44 +0200

    Optimization & padEnd fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Letter mapping for french phonetics, see https://fr.wikipedia.org/wiki/Soundex#Description
 const codes = {
   B: 1, P: 1,
-  C: 2, K: 2, Q: 2,
+  C: 2, Ç: 2, K: 2, Q: 2,
   D: 3, T: 3,
   L: 4,
   M: 5, N: 5,
@@ -9,7 +9,10 @@ const codes = {
   G: 7, J: 7,
   X: 8, Z: 8, S: 8,
   F: 9, V: 9,
-  A: 0, E: 0, I: 0, O: 0, U: 0,
+  A: 0, À: 0, Â: 0, Ä: 0,
+  E: 0, É: 0, È: 0, Ê: 0, Ë: 0,
+  I: 0, Î: 0, Ï: 0, O: 0,
+  Ô: 0, Ö: 0, U: 0, Û: 0, Ü: 0, Ù: 0,
 };
 
 //-------
@@ -17,10 +20,10 @@ const codes = {
 //-------
 module.exports = function (str, length = 4) {
   // Capitalize and remove accents
-  const buffer = String(str).normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase();
+  const buffer = String(str).toUpperCase();
 
   // Init result
-  let result = buffer.charAt(0);
+  let result = buffer.charAt(0).normalize('NFD').replace(/[\u0300-\u036f]/g, '');
   let previous = codes[result];
 
   // Map all chars
@@ -29,20 +32,19 @@ module.exports = function (str, length = 4) {
     const letter = buffer.charAt(i);
     const code = codes[letter];
 
-    // Ignore non letter character and apply the rule:
-    // "two letters with the same number separated by 'h' or 'w' are coded as a single number"
-    if (code === undefined || letter === 'H' || letter === 'W') {
+    // Ignore letter
+    if (code === undefined) {
       continue;
     }
 
     // Append code if different than previous
     if(code && code !== previous) {
       result += code;
-    }
 
-    // Soundex key is fulfilled ?
-    if (result.length === length) {
-      return result;
+      // Soundex key is fulfilled ?
+      if (result.length === length) {
+        return result;
+      }
     }
 
     // Save previous for next loop
@@ -50,5 +52,5 @@ module.exports = function (str, length = 4) {
   }
 
   // Fill with 0
-  return result.padEnd(length, '0');
+  return result.padEnd(4, '0');
 };
